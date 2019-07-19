3ff07c6ff7604d4e242aebb75e93426afddfe3a5
JosephCorwin <corwin.joe@gmail.com>
2019-07-19 05:02:19 -0500

    check-ins running smoothly

--- app/assets/javascripts/check_ins.js
+++ app/assets/javascripts/check_ins.js
@@ -3,8 +3,8 @@ jQuery(function(){
 	var batts;
 	var chars;
 
-    $('#check_in_battery_id', '#check_in_charger_id').parent().hide();
-
+    $('#check_in_battery_id').parent().hide();
+    $('#check_in_charger_id').parent().hide();
 	batts = $('#check_in_battery_id').html();
 	chars = $('#check_in_charger_id').html();
 	console.log(batts);
@@ -15,9 +15,9 @@ jQuery(function(){
 		var rider, batt_options, char_options;
 
 		rider = $('#check_in_rider_id :selected').text();
-		console.log(rider)
-		batt_options = $(batts).filter("optgroup[label=" + rider + "]").html();
-		char_options = $(chars).filter("optgroup[label=" + rider + "]").html();
+		console.log('optgroup[label="' + rider + '"]')
+		batt_options = $(batts).filter('optgroup[label="' + rider + '"]').html();
+		char_options = $(chars).filter('optgroup[label="' + rider + '"]').html();
 
 		console.log(batt_options);
 		console.log(char_options);
@@ -26,12 +26,12 @@ jQuery(function(){
 	
 			$('#check_in_battery_id').html(batt_options);
 			$('#check_in_charger_id').html(char_options);
-				return $('#check_in_battery_id','#check_in_charger_id').parent().show();
-
+				return  $('#check_in_battery_id').parent().show(),
+    					$('#check_in_charger_id').parent().show();
 		} else {
 
-			$('#check_in_battery_id', '#check_in_charger_id').empty();
 			return $('#check_in_battery_id', '#check_in_charger_id').parent().hide();
+
 		};
     });
 });
