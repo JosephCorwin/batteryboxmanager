c645db5e4bbfdc07f4d4e8cbd239f80c126e4d99
JosephCorwin <corwin.joe@gmail.com>
2019-07-19 23:15:12 -0500

    javascript for user-friendlier maunal check-ins is working. Still has a visible flash, but after turning off //=require_tree for application.js, the code is now running reliably on the DOM after it loads

--- app/assets/javascripts/check_ins.js
+++ app/assets/javascripts/check_ins.js
@@ -1,28 +1,33 @@
-jQuery(function(){
+$(document).ready(function(){
+
+    $('#check_in_battery_id').parent().hide();
+    $('#check_in_charger_id').parent().hide();
 
 	var batts;
 	var chars;
 
-    $('#check_in_battery_id').parent().hide();
-    $('#check_in_charger_id').parent().hide();
 	batts = $('#check_in_battery_id').html();
 	chars = $('#check_in_charger_id').html();
-	console.log(batts);
+	
+	console.log('farts');
 	console.log(chars);
 
 	$('#check_in_rider_id').change(function() {
 
-		var rider, batt_options, char_options;
+		var rider;
+		var batt_options;
+		var char_options;
 
 		rider = $('#check_in_rider_id :selected').text();
 		console.log('optgroup[label="' + rider + '"]')
+		
 		batt_options = $(batts).filter('optgroup[label="' + rider + '"]').html();
 		char_options = $(chars).filter('optgroup[label="' + rider + '"]').html();
 
 		console.log(batt_options);
 		console.log(char_options);
 
-		if (batt_options, char_options) {
+		if (batt_options || char_options) {
 	
 			$('#check_in_battery_id').html(batt_options);
 			$('#check_in_charger_id').html(char_options);
@@ -30,7 +35,8 @@ jQuery(function(){
     					$('#check_in_charger_id').parent().show();
 		} else {
 
-			return $('#check_in_battery_id', '#check_in_charger_id').parent().hide();
+			return $('#check_in_battery_id').parent().hide(),
+				   $('#check_in_charger_id').parent().hide();
 
 		};
     });
